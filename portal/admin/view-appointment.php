a994032edc8bd74304fdc32efc4148342c728c26
love <jasonreformado8@gmail.com>
2024-02-05 19:34:04 +0800

    modified:   portal/admin/view-appointment.php

--- portal/admin/view-appointment.php
+++ portal/admin/view-appointment.php
@@ -55,7 +55,7 @@ if (isset($_POST['submit'])) {
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Villa Arcadia | View Reservation</title>
+<title>Villa Arcadia | View Reservation </title>
 
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!-- Bootstrap Core CSS -->
